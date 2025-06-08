3fd3eb72116377a478118b5ddeb0f4760c30b4a6
Kar98Tik <kartikprayagi0@gmail.com>
2025-06-08 13:12:06 +0530

    bot added

---
+++ bot.js
@@ -0,0 +1,21 @@
+// bot.js
+const mineflayer = require('mineflayer');
+
+const bot = mineflayer.createBot({
+  host: 'Badmosh-w5e8.aternos.me', // Your Aternos server address
+  port: 21893,                    // Default Minecraft port
+  username: 'Vivanj',       // Use a Minecraft alt account
+  version: '1.20.1'              // Match your server version
+});
+
+bot.on('chat', (username, message) => {
+  if (username !== bot.username) {
+    bot.chat(`Hello ${username}!`);
+  }
+});
+
+bot.on('end', () => {
+  console.log('Bot disconnected, reconnecting...');
+  setTimeout(() => require('./bot'), 5000); // Auto-reconnect
+});
+setInterval(() => bot.setControlState('jump', true), 60000);
